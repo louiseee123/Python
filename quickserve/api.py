ee6968b1f1317f2f0996f2d4a4323d865e6551be
johnlouisebergabena <johnlouisebergs123@gmail.com>
2026-03-03 19:30:49 +0800

    Fix Vercel deployment with Werkzeug dispatcher

--- quickserve/api.py
+++ quickserve/api.py
@@ -2,7 +2,8 @@ import os
 import sys
 
 # Add the project directory to the path
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, BASE_DIR)
 
 # Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickserve.settings')
@@ -10,5 +11,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickserve.settings')
 # Import the WSGI application
 from quickserve.wsgi import application
 
+# For Vercel serverless, we need to wrap the WSGI app
+from werkzeug.wsgi import DispatcherMiddleware
+
 # Vercel requires the application to be called 'app'
-app = application
+app = DispatcherMiddleware(application, {
+    '/': application
+})
